a4a218169ce2a00351574a8f87b311ff481632ee
rubenavaldez <rubenavaldez@yahoo.com>
2022-06-19 13:02:48 -0700

    attempt 3

---
+++ srping5webapp/src/main/java/com/example/srping5webapp/Srping5webappApplication.java
@@ -0,0 +1,13 @@
+package com.example.srping5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srping5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Srping5webappApplication.class, args);
+	}
+
+}
---
+++ srping5webapp/src/test/java/com/example/srping5webapp/Srping5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srping5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srping5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
